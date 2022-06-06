21b9efa365b66f7afe5dbe5a71ed05ab5de4622a
sz0320-bit <yknse27@gmail.com>
2022-06-06 10:13:40 -0400

    added support for enter key and transitioned the onclick function only in javascript

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -1,9 +1,20 @@
 let button = document.querySelector('#mainsubmit');
-let textbox = document.querySelector('.textenter');
+let textbox = document.querySelector('#mainentry');
 let items = [];
-function clickd(){
-    let itemvalue = document.querySelector("#mainentry").value;
+
+button.addEventListener('click',() => {
+    let itemvalue = textbox.value;
     items.push(itemvalue);
+    textbox.value = "";
     console.log(itemvalue);
     console.log(JSON.stringify(items));
-}
+});
+textbox.addEventListener('keydown', (event) => {
+   if( event.keyCode == 13){
+       let itemvalue = textbox.value;
+       items.push(itemvalue);
+       textbox.value = "";
+       console.log(itemvalue);
+       console.log(JSON.stringify(items));
+   }
+});

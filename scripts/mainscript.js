8adf890d7b58f81ca121e261ce01d71b6f0c8f05
sz0320-bit <yknse27@gmail.com>
2022-06-06 13:24:09 -0400

    programmed the delete button, everything is now programmed, just need to finish styling and we are done, maybe as a side project ill program an edit button

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -24,21 +24,9 @@ textbox.addEventListener('keydown', (event) => {
 function displayList(x){
     let arraysize = items.length;
 let linebox = document.querySelector("#entrypoint");
-linebox.innerHTML += '<div class="textholder"><div class="unline" id="line'+arraysize+'">'+x+'</div><div class="buttonholder" id="holder'+arraysize+'"> <input type="button" value="delete" class="deletebutton" id="delete'+arraysize+'"></div></div>'
+linebox.innerHTML += '<div class="textholder"><div class="unline" id="line'+arraysize+'">'+x+'</div><div class="buttonholder" id="holder'+arraysize+'"> <input type="button" value="delete" class="deletebutton" id="delete'+arraysize+'" onclick="this.parentNode.parentNode.remove();"></div></div>'
     console.log()
 }
-/*
-let deleted = document.querySelector(".deletebutton");
 
-document.querySelector(".deletebutton").addEventListener('click', () =>{
-    let deleteid = deleted.id-'delete';
-    console.log(deleteid);
-    //let linebox = document.querySelector("#entrypoint");
-    //let bullet = document.querySelector('#line'+deleteid);
-   // let box = document.querySelector('#holder'+deleteid);
-   // let buttn = document.querySelector('#delete'+deleteid);
-    //console.log((buttn+bullet+box));
-    //linebox.remove(bullet);
-   // linebox.remove(box);
-   // linebox.remove(buttn);
-});*/
+let deleted = document.querySelector(".deletebutton");
+console.log(deleted)

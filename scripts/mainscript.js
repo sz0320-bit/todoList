37bcfca102da9cb6a7744ea7c365fe073139043c
sz0320-bit <yknse27@gmail.com>
2022-06-06 11:06:45 -0400

    programmed the display list when value is entered, just need to finish some extra css and program delete button

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -6,15 +6,22 @@ button.addEventListener('click',() => {
     let itemvalue = textbox.value;
     items.push(itemvalue);
     textbox.value = "";
+    displayList(itemvalue);
     console.log(itemvalue);
     console.log(JSON.stringify(items));
 });
 textbox.addEventListener('keydown', (event) => {
-   if( event.keyCode == 13){
+   if( event.keyCode === 13){
        let itemvalue = textbox.value;
        items.push(itemvalue);
+       displayList(itemvalue);
        textbox.value = "";
        console.log(itemvalue);
        console.log(JSON.stringify(items));
    }
 });
+
+function displayList(x){
+let linebox = document.querySelector("#entrypoint");
+linebox.innerHTML += '<li class="unline">'+x+'</li><div class="buttonholder"> <input type="button" value="delete" class="deletebutton"></div>'
+}

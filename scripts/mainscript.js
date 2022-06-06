fda95df0c5fadb156dd8ca3968dd86366621bb3e
sz0320-bit <yknse27@gmail.com>
2022-06-06 13:09:49 -0400

    changed it so that the elements are now in a div instead of li element

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -22,6 +22,23 @@ textbox.addEventListener('keydown', (event) => {
 });
 
 function displayList(x){
+    let arraysize = items.length;
 let linebox = document.querySelector("#entrypoint");
-linebox.innerHTML += '<li class="unline">'+x+'</li><div class="buttonholder"> <input type="button" value="delete" class="deletebutton"></div>'
+linebox.innerHTML += '<div class="textholder"><div class="unline" id="line'+arraysize+'">'+x+'</div><div class="buttonholder" id="holder'+arraysize+'"> <input type="button" value="delete" class="deletebutton" id="delete'+arraysize+'"></div></div>'
+    console.log()
 }
+/*
+let deleted = document.querySelector(".deletebutton");
+
+document.querySelector(".deletebutton").addEventListener('click', () =>{
+    let deleteid = deleted.id-'delete';
+    console.log(deleteid);
+    //let linebox = document.querySelector("#entrypoint");
+    //let bullet = document.querySelector('#line'+deleteid);
+   // let box = document.querySelector('#holder'+deleteid);
+   // let buttn = document.querySelector('#delete'+deleteid);
+    //console.log((buttn+bullet+box));
+    //linebox.remove(bullet);
+   // linebox.remove(box);
+   // linebox.remove(buttn);
+});*/

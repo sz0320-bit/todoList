8388f6034b6c838785a7fa8512d5afe1c07a212c
sz0320-bit <yknse27@gmail.com>
2022-06-10 08:58:48 -0400

    all functionality added, styling is incomplete

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -7,7 +7,6 @@ button.addEventListener('click',() => {
     items.push(itemvalue);
     textbox.value = "";
     displayList();
-    console.log(itemvalue);
     console.log(JSON.stringify(items));
 });
 textbox.addEventListener('keydown', (event) => {
@@ -25,14 +24,15 @@ function displayList(){
     let arraysize = items.length;
 let linebox = document.querySelector("#entrypoint");
 
-    linebox.innerHTML += '<div class="textholder">' +
-        '<div class="unline" id="line' + arraysize + '">' +
-        '<input type="text" readonly value="' + items[arraysize-1] + '">' +
-        '</div>' +
-        '<div class="buttonholder" id="holder' + arraysize + '">' +
-        ' <input type="button" value="edit" class="editbutton" id="edit' + arraysize + '" onclick="edits(this)">' +
-        '<input type="button" value="delete" class="deletebutton" id="delete' + arraysize + '" onclick="deletes(this);">' +
-        '</div>' +
+    linebox.innerHTML +=
+        '<div class="textholder">' +
+            '<div class="unline" id="line' + (arraysize-1) + '" >' +
+                '<input type="text" readonly value="' + items[arraysize-1] + '" onfocusout="setsData(this);">' +
+            '</div>' +
+            '<div class="buttonholder" id="holder' + arraysize + '">' +
+                ' <input type="button" value="edit" class="editbutton" id="edit' + arraysize + '" onclick="edits(this);">' +
+                '<input type="button" value="delete" class="deletebutton" id="delete' + arraysize + '" onclick="deletes(this);">' +
+            '</div>' +
         '</div>'
     console.log()
 
@@ -44,4 +44,11 @@ function deletes(x){
 
 function edits(x){
     x.parentNode.parentNode.firstChild.firstChild.removeAttribute('readonly');
+}
+
+function setsData(x){
+    const lineId = x.parentNode.id.replace("line","");
+    items[lineId] = x.value;
+    console.log(items)
+    x.setAttribute("readonly",true);
 }
